9b0bf7873067335cc925bd60cc70dd868aeb14cb
Stro <eduardo.straub@gmail.com>
2016-04-20 20:31:40 -0300

    Ahora se le agrega el tiempo de ejecución.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include "tsptabusolver.h"
+#include <ctime>
+#include<stdio.h>
+
 using namespace std;
 
 
 int main(int argc, char* argv[]){
 
-	TSPTabuSolver solver2("tsp0.txt");
-	solver2.solve(6);	
-	TSPTabuSolver solver1("tsp1.txt");
-	solver1.solve(5);				
+	std::clock_t start;
+  start = std::clock();
 
-				
+	TSPTabuSolver solver2("tsp0.txt");
+	solver2.solve(6);
+
+	TSPTabuSolver solver1("tsp1.txt");
+	solver1.solve(5);
 
 	TSPTabuSolver solver3("tsp2.txt");
-	solver3.solve(7);				
+	solver3.solve(7);
+
+	std::cout << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) <<
+	" ms" << std::endl;
 	
 	return 1;
 }
